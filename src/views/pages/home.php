5063df61f214b918e61207012c49fddf5a775d70
Betinho Rebouças <betinhoreboucas91@gmail.com>
2022-05-19 11:07:20 -0300

    Página inicial em construção

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,3 +1,30 @@
 <?php $render('header'); ?>
 
-Opa, <?=$nome;?>
+
+<div class="content">
+    <div class="geral">
+        <div>
+            Desempenho Geral
+        </div>
+        <div>
+            <div>
+                <form method="get">
+                    <select name="disciplina">
+                        <option value="">Escolher Disciplinas</option>
+                    </select>
+                    <input type="date" name="dataInicial">
+                    <input type="date" name="dataFinal">
+                    <input type="button" value="Gerar">
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="info">
+
+    </div>
+</div>
+
+
+
+</body>
+</html>
