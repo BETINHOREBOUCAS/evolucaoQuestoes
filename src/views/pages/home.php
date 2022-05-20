a8bb940ff47cf0d9e73f26cb6ec53810e83840cd
Betinho Rebouças <betinhoreboucas91@gmail.com>
2022-05-20 11:09:13 -0300

    produção pagina principal

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -3,28 +3,73 @@
 
 <div class="content">
     <div class="geral">
-        <div>
-            Desempenho Geral
-        </div>
-        <div>
-            <div>
-                <form method="get">
+        <div class="form-container">
+            <form method="get">
+                <div class="form">
                     <select name="disciplina">
-                        <option value="">Escolher Disciplinas</option>
+                        <option value="">Geral</option>
+                        <option value="administrativo">Direito Administrativo</option>
                     </select>
-                    <input type="date" name="dataInicial">
-                    <input type="date" name="dataFinal">
+
+                    <input type="date" name="dataInicial" title="Período Inicial">
+
+                    <input type="date" name="dataFinal" title="Período Final">
+
                     <input type="button" value="Gerar">
-                </form>
+                </div>
+            </form>
+        </div>
+        <hr>
+        <div class="result">
+            <div class="result-content">
+                <div class="result-title">Total de Resoluções</div>
+                <div class="result-value">10</div>
+            </div>
+            <div class="result-content">
+                <div class="result-title">Resoluções Corretas</div>
+                <div class="result-value">10</div>
+            </div>
+            <div class="result-content">
+                <div class="result-title">Resoluções Erradas</div>
+                <div class="result-value">10</div>
+            </div>
+            <div class="result-content">
+                <div class="result-title">Taxa de erros</div>
+                <div class="result-value">10%</div>
+            </div>
+            <div class="result-content">
+                <div class="result-title">Taxa de acerto</div>
+                <div class="result-value">10%</div>
             </div>
         </div>
+        <div>
+            <table border="1">
+                <tr>
+                    <th>Company</th>
+                    <th>Contact</th>
+                    <th>Country</th>
+                </tr>
+                <tr>
+                    <td>Alfreds Futterkiste</td>
+                    <td>Maria Anders</td>
+                    <td>Germany</td>
+                </tr>
+                <tr>
+                    <td>Centro comercial Moctezuma</td>
+                    <td>Francisco Chang</td>
+                    <td>Mexico</td>
+                </tr>
+            </table>
+        </div>
     </div>
-    <div class="info">
 
+
+    <div class="info">
     </div>
 </div>
 
 
 
 </body>
+
 </html>
